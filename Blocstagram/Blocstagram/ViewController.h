fc1939d6b8749a87d58ef56cad43eb3c37ce2305
Jack Li <drjackl@gmail.com>
2016-02-27 22:15:43 -0800

    Deleted storyboard and its references

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Jack Li on 2/27/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Jack Li on 2/27/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
